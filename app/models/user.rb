5025ce788657a7546eae630dbf659a7cf3af22da
azheng249 <andy.zheng249@gmail.com>
2016-10-11 18:03:00 -0400

    Added presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User
+  # Attributes
   include Mongoid::Document
   field :first_name,  type: String
   field :last_name,   type: String
   field :email,       type: String
 
+  # Validations
   validates_presence_of :first_name, :last_name
+
+  embeds_many :addresses
 end
